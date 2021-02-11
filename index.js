05f22294ae7b23b15553770f52736bc0624c6927
Tushar <tushar.mca17.du@gmail.com>
2021-02-11 18:11:03 +0530

    khj

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const config = {
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
-    ping_timeout: 60,
-    allow_origin: '*'
+    ping_timeout: 60
   },
   http: {
-    port: process.env.PORT || 8000,
+    port: 8000,
     allow_origin: '*'
   }
 };
